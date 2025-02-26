5def24321939adc6000065d4e0187d03323a7090
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2025-02-26 18:38:50 -0300

    feat: changed

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,26 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="font-serif p-20 max-w-lg space-y-6">
+    <h1 class="text-7xl">Hello!</h1>
+
+    <h2 class="text-4xl tracking-tight">I'm Victor.</h2>
+
+    <p>
+      I help architects and engineers design buildings using the next generation tools for
+      architectural design. I am currently based in Copenhagen working with a global team at
+      Perkins&Will and Schimidt Hammer Lassen.
+    </p>
+
+    <p>
+      I started my journey in design technology through Computational Design using Rhino and
+      Grasshopper since 2018. Since then I have transitioned to web development and developing
+      extensions to Rhino and Revit.
+    </p>
+
+    <p>
+      You can check some of my public work at Github, or follow me on LinkedIn where I post about
+      design technology in the AEC industry.
+    </p>
+  </div>
 </template>
