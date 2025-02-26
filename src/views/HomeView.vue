7759084022b9402ebcd09159d407ad3b448f3992
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2025-02-26 19:48:27 -0300

    feat: adjusted text, style for buttons and page title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,30 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="font-serif p-20 max-w-lg space-y-6">
-    <h1 class="text-7xl">Hello!</h1>
+  <main>
+    <h1 class="text-7xl font-serif">Hello!</h1>
 
-    <h2 class="text-4xl tracking-tight">I'm Victor.</h2>
+    <h2 class="text-4xl tracking-tight font-serif">I'm Victor.</h2>
 
     <p>
       I help architects and engineers design buildings using the next generation tools for
       architectural design. I am currently based in Copenhagen working with a global team at
-      Perkins&Will and Schimidt Hammer Lassen.
+      <a href="https://perkinswill.com" target="_blank">Perkins&Will</a> and
+      <a href="https://shl.dk" target="_blank">Schmidt Hammer Lassen</a>.
     </p>
 
     <p>
-      I started my journey in design technology through Computational Design using Rhino and
-      Grasshopper since 2018. Since then I have transitioned to web development and developing
+      I started my design technology journey through Computational Design using Rhino and
+      Grasshopper around 2018. Since then I have transitioned to web development and building
       extensions to Rhino and Revit.
     </p>
 
     <p>
-      You can check some of my public work at Github, or follow me on LinkedIn where I post about
-      design technology in the AEC industry.
+      You can check some of my public work at
+      <a href="http://github.com/vwnd" target="_blank" rel="noopener noreferrer">Github</a>, or
+      follow me on
+      <a href="https://www.linkedin.com/in/victorwanderley/" target="_blank">LinkedIn</a> where I
+      post about design technology in the AEC industry.
     </p>
-  </div>
+  </main>
 </template>
