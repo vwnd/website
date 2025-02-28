97fcf5bc6d337e6049ff0015c112773d8714029b
Victor Wanderley Barbosa <61347148+vwnd@users.noreply.github.com>
2025-02-28 14:16:51 -0300

    feat: added locale

---
+++ src/locale/i18n.ts
@@ -0,0 +1,30 @@
+import type { I18nOptions } from 'vue-i18n'
+
+const options: I18nOptions = {
+  legacy: false,
+  locale: 'en-US',
+  messages: {
+    en: {
+      hello: 'Hi there!',
+      intro: "I'm Victor, nice to meet you 👋",
+      whoami:
+        ' I help architects and engineers design buildings using the next generation tools for architectural design. I am currently based in Copenhagen working with a global team at {perkinswill} and {shl}.',
+      journey:
+        'I started my design technology journey through Computational Design using Rhino and Grasshopper around 2018. Since then I have transitioned to web development and building extensions to Rhino and Revit.',
+      references:
+        'You can check some of my public work at {github}, or follow me on {linkedin}, where I post about design technology in the AEC industry',
+    },
+    'pt-BR': {
+      hello: 'Olá!',
+      intro: 'Eu sou Victor, prazer em conhecê-lo 👋',
+      whoami:
+        'Eu ajudo arquitetos e engenheiros a projetar edifícios usando as ferramentas de design arquitetônico da próxima geração. Atualmente moro em Copenhague trabalhando com uma equipe global na {perkinswill} e {shl}.',
+      journey:
+        'Comecei minha jornada em tecnologia de design através do Design Computacional usando Rhino e Grasshopper por volta de 2018. Desde então, fiz a transição para o desenvolvimento web e a construção de extensões para Rhino e Revit.',
+      references:
+        'Você pode conferir alguns dos meus trabalhos públicos no {github}, ou me seguir no {linkedin}, onde posto sobre tecnologia de design na indústria AEC',
+    },
+  },
+}
+
+export default options
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createApp } from 'vue'
 
+import { createI18n } from 'vue-i18n'
 import App from './App.vue'
+import localeOptions from './locale/i18n'
 import router from './router'
+
+const i18n = createI18n(localeOptions)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(i18n)
 
 app.mount('#app')
